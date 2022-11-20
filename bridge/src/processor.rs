b176ef25ad88713c9e05d5d455879bdc837239f6
Benja Heart <visopsys@users.noreply.github.com>
2022-11-20 10:44:41 -0800

    Add bridge test

--- bridge/src/processor.rs
+++ bridge/src/processor.rs
@@ -225,3 +225,53 @@ impl Processor {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use {
+        super::*,
+        assert_matches::*,
+        solana_program::instruction::{AccountMeta, Instruction},
+        solana_program_test::*,
+        solana_sdk::{signature::Signer, transaction::Transaction},
+    };
+
+    #[tokio::test]
+    async fn test_initialize() {
+        let program_id = Pubkey::new_unique();
+        let seed_string = b"SisuBridge";
+        let (bridge_pda, _) = Pubkey::find_program_address(&[seed_string], &program_id);
+
+        let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
+            "sisu_bridge",
+            program_id,
+            processor!(Processor::process_instruction),
+        )
+        .start()
+        .await;
+
+        let data = BridgeInstruction::try_to_vec(&BridgeInstruction::Initialize).unwrap();
+        let mut transaction = Transaction::new_with_payer(
+            &[Instruction {
+                program_id,
+                accounts: vec![
+                    AccountMeta::new(payer.pubkey(), false),
+                    AccountMeta::new(bridge_pda, false),
+                    AccountMeta::new(system_program::id(), false),
+                ],
+                data,
+            }],
+            Some(&payer.pubkey()),
+        );
+        transaction.sign(&[&payer], recent_blockhash);
+        assert_matches!(banks_client.process_transaction(transaction).await, Ok(()));
+
+        // Make sure the new
+        assert_matches!(banks_client.get_account(bridge_pda).await, Ok(_));
+
+        let account = banks_client.get_account(bridge_pda).await.unwrap().unwrap();
+
+        let state = BridgeStateV0::try_from_slice(account.data.as_slice()).unwrap();
+        assert_eq!(payer.pubkey(), state.admin);
+    }
+}
